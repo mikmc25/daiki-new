e4af9bb49236606bf3e1ee7dbc3e062874f17e0c
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-04-27 02:32:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -651,9 +651,9 @@ app
 
     //
     var json = {
-      id: "daiki.jackettrdygg.stream",
+      id: "hy.jackettrdtl.stream",
       version: "1.0.2",
-      name: "Solid Torrents Addon",
+      name: "Torrentleech Addon",
       description: "Movie & TV Streams from Solid Torrents",
       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3rgndsSZo5RmTgC_Vv0xo8Ea5vlv2Ojx5e3IMahlLz05hm2XlpBxa-no1sqiRIziYIBI&usqp=CAU",
       resources: [
