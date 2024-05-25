2435ed7af587b9f4b0f68bc642b8a88cbf684df1
mikmc25 <167842116+mikmc25@users.noreply.github.com>
2024-05-25 16:11:45 +0100

    Update iptorrindex.js

--- iptorrindex.js
+++ iptorrindex.js
@@ -515,7 +515,7 @@ let fetchTorrent = async (query, type = "series") => {
 
   let url = `${
     hostdata.host
-  }/api/v2.0/indexers/iptorrents/results/torznab/api?apikey=${
+  }/api/v2.0/indexers/pretome/results/torznab/api?apikey=${
     hostdata.apiKey
   }&${
     type == "series" ? "t=tvsearch" : type == "movie" ? "t=movie" : ""
